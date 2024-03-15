e3097958dff5e4f9a2708007561c0fbeffa0ffdf
Jessicazzzzzz <86815288+Jessicazzzzzz@users.noreply.github.com>
2024-03-15 16:52:53 +1100

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-undef
+module.exports= {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+  "eslint:recommended",
+  "plugin:react/recommended",
+  "plugin:@typescript-eslint/recommended",
+  
+  ], 
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "parser": "@typescript-eslint/parser",
+  "plugins": [
+    "@typescript-eslint",
+    "react",
+    "react-hooks",
+    "prettier"
+  ],
+  "rules": {
+  
+}
+}
